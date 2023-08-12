3ad7cb75376afa8242076f3f2c4e2d4f004796f7
WJerry11 <136723953+WJerry11@users.noreply.github.com>
2023-08-12 17:11:07 +0000

    first commit

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoHeader from "./TodoHeader";
+import TodoBody from "./TodoBody";
+import TodoFooter from "./todofooter";
+
+const TodoList = () => {
+  return (
+    <>
+      <TodoHeader />
+      <TodoFooter />
+    </>
+  );
+};
+export default TodoList;
